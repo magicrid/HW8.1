f0f6003e999577dec9666d65ce7ef707af765381
magicrid <bmanilenko@gmail.com>
2025-09-30 22:52:07 +0300

    Add files via upload

---
+++ HW8.1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+void counting_function() 
+{
+    static int sum{ 1 };
+
+    std::cout << "Количество вызовов функции counting_function(): " << sum << std::endl;
+    sum++;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    for (int i = 0; i < 15; i++)
+    {
+        counting_function();
+    }
+
+    return EXIT_SUCCESS;
+}
